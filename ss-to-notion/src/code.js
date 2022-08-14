aae83990c3d9590efcbc69d64d4c78130854cc4f
Fuya Makino <fuyafuya1224@gmail.com>
2022-08-15 02:06:25 +0900

    feat: notion -> gasの接続ができることを確認

--- ss-to-notion/src/code.js
+++ ss-to-notion/src/code.js
@@ -1,71 +1,58 @@
 function main() {
-  const ss = SpreadsheetApp.getActiveSpreadsheet();
-  const js = convertJson(ss.getDataRange().getValues());
-
-  js.forEach((elem, index) => {
-    const obj = {
-      parent: {
-        database_id: DATABASE_ID,
-      },
-      properties: {
-        Name: {
-          title: [
-            {
-              text: {
-                content: elem.Name,
-              },
-            },
-          ],
-        },
-        Description: {
-          rich_text: [
-            {
-              text: {
-                conetent: elem.Description,
-              },
-            },
-          ],
-        },
-        Date: {
-          type: "date",
-          date: {
-            start: Utilities.formatDate(elem.Date, "JST", "yyyy-MM-dd"),
-            end: null,
-          },
-        },
-      },
-    };
-    console.log(obj);
-    postNotion(obj);
-    Utilities.sleep(1000);
-  });
+  const res = getNotionDbData();
+  console.log(res);
 }
 
-const postNotion = (obj) => {
-  const options = {
-    method: "post",
-    headers: {
-      'Content-Type':'application/json',
-      'Notion-Version':'2021-05-13',
-      'Autorization':"Bearer "+NOTION_TOKEN,
-    },
-    payload: JSON.stringify(obj),
+const notion = (text) => {
+  const url = 'https://api.notion.com/v1/pages';
+  const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + NOTION_TOKEN,
+    'Notion-Version': '2022-06-28',
   };
-  console.log(options);
-  var response = UrlFetchApp.fetch(`https://api.notion.com/v1/pages`, options);
-  console.log(response);
-};
 
-const convertJson = (range) => {
-  const key = range[0];
+  const postData = {
+    "parent": {
+      "type": "database_id",
+      "database_id": DATABASE_ID
+    },
+    "properties": {
+      "Name": {
+        "title": [
+          {
+            "text": {
+              "content": text,
+            }
+          }
+        ]
+      }
+    }
+  };
 
-  const js = range.slice(1).map((row) => {
-    const obj = {};
-    row.map((item, index) => {
-      obj[String(key[index])] = item;
-    });
-    return obj;
-  });
+  const options = {
+    "method": "post",
+    "headers": headers,
+    "muteHttpExceptions" : true,
+    "payload": JSON.stringify(postData),
+  };
 
-  return js;
-};
+  return UrlFetchApp.fetch(url, options);
+}
+
+const getNotionDbData = () => {
+  const url = `https://api.notion.com/v1/databases/${DATABASE_ID}/query`;
+
+  let headers = {
+    'content-type' : 'application/json; charset=UTF-8',
+    'Authorization': 'Bearer ' + NOTION_TOKEN,
+    'Notion-Version': '2022-06-28',
+  };
+  let options ={
+    'method': 'post',
+    'headers': headers,
+  }
+
+  const notionData = UrlFetchApp.fetch(url, options);
+  const result = JSON.parse(notionData);
+  return result;
+}
