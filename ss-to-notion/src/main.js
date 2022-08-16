da46a9742984659421e1b55c307aceb0aa51dd1f
Fuya Makino <fuyafuya1224@gmail.com>
2022-08-17 04:25:03 +0900

    feat: スプシの状態をnotionに同期

--- ss-to-notion/src/main.js
+++ ss-to-notion/src/main.js
@@ -1,27 +1,62 @@
 function main() {
-  const postData = {
-    "parent": { "database_id": DATABASE_ID },
-    "properties": {
-      "Name": {
-        "title": [
-          {
-            "text": {
-              "content": "test_man1"
-            }
-          }
-        ]
-      },
-      "tag1": {
-        "rich_text": [
-          {
-            'text': {
-              'content': 'testtt'
-            }
-          }
-        ]
+  const ss = SpreadsheetApp.getActiveSpreadsheet();
+  const range = ss.getDataRange().getValues();
+  const jsonData = convertJson(range)
+  postData(jsonData);
+}
+
+const convertJson = (range) =>  {
+  const key = range[0];
+
+  const jsonData = range.slice(1).map(
+    function (row) {
+      const obj = {};
+      row.map(function (item, index) {
+        obj[String(key[index])] = item;
       }
-    }
-  };
-  const res = postNotionDb(postData);
-  console.log(res);
+      );
+      return obj;
+    });
+  return jsonData;
+}
+
+const postData = (data) => {
+  data.forEach((elem) => {
+    const obj = {
+      'parent': {
+        'database_id': DATABASE_ID
+      },
+      'properties': {
+        'Name': {
+          'title': [
+            {
+              'text': {
+                'content': elem.Name
+              }
+            }
+          ]
+        },
+        'Date': {
+          'type': 'date',
+          'date': {
+            'start': Utilities.formatDate(elem.Date, 'JST', 'yyyy-MM-dd'),
+            'end': null
+          }
+        },
+        'Description': {
+          'rich_text': [
+            {
+              'text': {
+                'content': elem.Description
+              }
+            }
+          ]
+        }
+      }
+    };
+
+    const res = postNotionDb(obj);
+    Utilities.sleep(1000);
+    console.log(res)
+  })
 }
--- ss-to-notion/src/ss.js
+++
@@ -1,5 +0,0 @@
-const getSpreadSheet = () => {
-  const ss = SpreadsheetApp.getActiveSpreadsheet();
-  const sheet = ss.getSheetByName("シート1");
-  return sheet;
-};
